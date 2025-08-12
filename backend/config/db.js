2fd5dcaa5ed55fb45b92f0396cd032d59958f1da
alex-miyake <alexandermiyake@btinternet.com>
2025-08-12 23:29:27 +0100

    configuring db connection for AWS RDS

--- backend/config/db.js
+++ backend/config/db.js
@@ -19,10 +19,10 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  process.env.PG_USER,
-  process.env.PG_PASSWORD,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
     logging: false,
--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.post('/test', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT || 3000;
 const HOST = '0.0.0.0';
 
 async function startServer() {
